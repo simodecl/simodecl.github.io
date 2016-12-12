5743eea087c8a9c313c0df6cf8909ff5e0fc5fd1
manatran <manatran@student.arteveldehs.be>
2016-12-12 21:04:48 +0100

    'update'

--- js/google_maps.js
+++ js/google_maps.js
@@ -1,58 +1,53 @@
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom:13,
-          center: new google.maps.LatLng(51.048017, 3.727666)
-        });
+var map;
 
-        // Create a <script> tag and set the USGS URL as the source.
-        var script = document.createElement('script');
-        script.src = 'js/ecoplan.geojson';
-        document.getElementsByTagName('head')[0].appendChild(script);
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: new google.maps.LatLng(51.048017, 3.727666)
+  });
 
-        var pos = {};
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-              pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            
-            var selfMarker = new google.maps.Marker({
-            position: new google.maps.LatLng(pos.lat,pos.lng),
-            title:"My location",
-            icon:'resources/images/markers/home.png',
-            map: map    
-            });// Create a Google Maps Marker  
-            selfMarker.setPosition(pos);
-            map.setCenter(pos);
-            return pos.lat;
-            return pos.lng;
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        }
+  var pos = {};
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-         window.ecoplan_callback = function(results) {
+      window.ecoplan_callback = function (results) {
         for (var i = 0; i < results.features.length; i++) {
           var coords = results.features[i].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var markers = [];
-          var d = Utils.calculateDistanceBetweenTwoCoordinates(latLng,pos.lat,pos.lng);
-          if(d <= 2) {
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            icon:'resources/images/markers/ecoplan.png'
-          });
-          markers.push(marker);
+          var latLng = new google.maps.LatLng(coords[1], coords[0]);
 
-        var options = {
-            imagePath: '/resources/images/markers/m'
-        };
-
-        var markerCluster = new MarkerClusterer(map, markers, options);
+          var radius = Utils.calculateDistanceBetweenTwoCoordinates(coords[1], coords[0], pos.lat, pos.lng);
+          if (radius <= 30) {
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map,
+              icon: 'resources/images/markers/ecoplan.png'
+            });
           };
         }
       }
-      }
+
+      
+      // Create a <script> tag and set the USGS URL as the source.
+      var script = document.createElement('script');
+      script.src = 'js/ecoplan.geojson';
+      document.getElementsByTagName('head')[0].appendChild(script);
+
+      var selfMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.lat, pos.lng),
+        title: "My location",
+        icon: 'resources/images/markers/home.png',
+        map: map
+      });// Create a Google Maps Marker  
+      selfMarker.setPosition(pos);
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  }
+
+
+}
