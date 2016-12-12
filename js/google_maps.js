3c4a567f6a026b808516cf45d4d549435019f497
manatran <manatran@student.arteveldehs.be>
2016-12-12 22:42:19 +0100

    'update'

--- js/google_maps.js
+++ js/google_maps.js
@@ -14,23 +14,25 @@ function initMap() {
         lng: position.coords.longitude
       };
 
+
       window.ecoplan_callback = function (results) {
         for (var i = 0; i < results.features.length; i++) {
           var coords = results.features[i].geometry.coordinates;
           var latLng = new google.maps.LatLng(coords[1], coords[0]);
 
           var radius = Utils.calculateDistanceBetweenTwoCoordinates(coords[1], coords[0], pos.lat, pos.lng);
-          if (radius <= 30) {
+          if (radius <= 5) {
             var marker = new google.maps.Marker({
               position: latLng,
               map: map,
               icon: 'resources/images/markers/ecoplan.png'
             });
+            return radius;
           };
         }
       }
 
-      
+
       // Create a <script> tag and set the USGS URL as the source.
       var script = document.createElement('script');
       script.src = 'js/ecoplan.geojson';
@@ -41,7 +43,17 @@ function initMap() {
         title: "My location",
         icon: 'resources/images/markers/home.png',
         map: map
-      });// Create a Google Maps Marker  
+      });
+      var cityCircle = new google.maps.Circle({
+            strokeColor: '#66CC66',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#66CC66',
+            fillOpacity: 0.25,
+            map: map,
+            center: { lat: pos.lat, lng: pos.lng },
+            radius: 5*1000
+          });
       selfMarker.setPosition(pos);
       map.setCenter(pos);
     }, function () {
