a76751713f569d051c81b73c79c02bce71c97c55
manatran <manatran@student.arteveldehs.be>
2016-12-12 23:14:16 +0100

    'update'

--- home.md
+++ home.md
@@ -6,36 +6,6 @@ title   : "Home"
 <div id="map"></div>
 <div class="nearby">
     <h1 class="title-green">Nearby</h1>
-    <div class="restaurant">
-        <h2 class="restaurant-name">Restaurant</h2>
-        <p class="restaurant-address">Restaurant address</p>
-    </div>
-    <div class="restaurant">
-        <h2 class="restaurant-name">Restaurant</h2>
-        <p class="restaurant-address">Restaurant address</p>
-    </div>
-    <div class="restaurant">
-        <h2 class="restaurant-name">Restaurant</h2>
-        <p class="restaurant-address">Restaurant address</p>
-    </div>
-    <div class="restaurant">
-        <h2 class="restaurant-name">Restaurant</h2>
-        <p class="restaurant-address">Restaurant address</p>
-    </div>
-    <div class="restaurant">
-        <h2 class="restaurant-name">Restaurant</h2>
-        <p class="restaurant-address">Restaurant address</p>
-    </div>
-    <div class="restaurant">
-        <h2 class="restaurant-name">Restaurant</h2>
-        <p class="restaurant-address">Restaurant address</p>
-    </div>
-    <div class="restaurant">
-        <h2 class="restaurant-name">Restaurant</h2>
-        <p class="restaurant-address">Restaurant address</p>
-    </div>
-    <div class="restaurant">
-        <h2 class="restaurant-name">Restaurant</h2>
-        <p class="restaurant-address">Restaurant address</p>
+    <div class="results">
     </div>
 </div>
--- js/google_maps.js
+++ js/google_maps.js
@@ -1,5 +1,5 @@
 var map;
-var radius = 5;
+var radius = 30;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -15,14 +15,20 @@ function initMap() {
         lng: position.coords.longitude
       };
 
-
+var tempStr = '';
       window.ecoplan_callback = function (results) {
         for (var i = 0; i < results.features.length; i++) {
           var coords = results.features[i].geometry.coordinates;
           var latLng = new google.maps.LatLng(coords[1], coords[0]);
-
           var distance = Utils.calculateDistanceBetweenTwoCoordinates(coords[1], coords[0], pos.lat, pos.lng);
           if (distance <= radius) {
+            
+            tempStr += '<div class="restaurant">';
+            tempStr += '<h2 class="restaurant-name">' + results.features[i].properties.NAAM + '</h2>';
+            tempStr += '<p class="restaurant-address">' + results.features[i].properties.STRAAT + ' ' + results.features[i].properties.NUMMER + '</p>';
+            tempStr += '<p class="distance">' + distance.toFixed(1) + ' km</p>';
+            tempStr += '</div>';
+
             var marker = new google.maps.Marker({
               position: latLng,
               map: map,
@@ -30,6 +36,7 @@ function initMap() {
             });
           };
         }
+        document.querySelector('.results').innerHTML = tempStr;
       }
 
 
@@ -45,15 +52,15 @@ function initMap() {
         map: map
       });
       var cityCircle = new google.maps.Circle({
-            strokeColor: '#66CC66',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#66CC66',
-            fillOpacity: 0.25,
-            map: map,
-            center: { lat: pos.lat, lng: pos.lng },
-            radius: radius*1000
-          });
+        strokeColor: '#66CC66',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#66CC66',
+        fillOpacity: 0.25,
+        map: map,
+        center: { lat: pos.lat, lng: pos.lng },
+        radius: radius * 1000
+      });
       selfMarker.setPosition(pos);
       map.setCenter(pos);
     }, function () {
