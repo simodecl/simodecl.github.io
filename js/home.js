4ebf3d5ab8099533b6a56456e98bb3317bf2033d
manatran <manatran@student.arteveldehs.be>
2016-12-15 16:41:04 +0100

    'update'

--- js/home.js
+++ js/home.js
@@ -21,4 +21,28 @@
             }, 333);
         });
     }
+
+    var bikecoords;
+    var bikelatLng;
+    var bikemarker;
+
+    Utils.getJSONByPromise('https://datatank.stad.gent/4/mobiliteit/bluebikedeelfietsensintpieters.json').then(function (fromResolve) {
+        bikecoords = fromResolve.geometry.coordinates;
+        bikelatLng = new google.maps.LatLng(bikecoords[1], bikecoords[0]);
+        bikemarker = new google.maps.Marker({
+            position: bikelatLng,
+            map: map,
+            icon: 'resources/images/markers/blue_bike.png'
+        });
+    });
+
+    Utils.getJSONByPromise('https://datatank.stad.gent/4/mobiliteit/bluebikedeelfietsendampoort.json').then(function (fromResolve) {
+        bikecoords = fromResolve.geometry.coordinates;
+        bikelatLng = new google.maps.LatLng(bikecoords[1], bikecoords[0]);
+        bikemarker = new google.maps.Marker({
+            position: bikelatLng,
+            map: map,
+            icon: 'resources/images/markers/blue_bike.png'
+        });
+    });
 })();
