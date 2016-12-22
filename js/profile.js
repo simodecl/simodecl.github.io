ca720f891d0158f483cb4b78a7875dc250a234b5
manatran <manatran@student.arteveldehs.be>
2016-12-22 19:39:26 +0100

    'update'

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,9 @@
 ; (function () {
     $('.fa-star').on('click', function(){
-        $(this).addClass('unfavourite');
+        $(this).toggleClass('fa-star-o');
+        //$(this).addClass('unfavourite');
         $(this).parent().delay(200).hide('400ms', 'linear');
+
     });
 
     if ($('#back-to-top').length) {
